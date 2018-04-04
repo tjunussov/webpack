9f1e0dc33025e9d742aaf812016f82ffd13b942a
Timur <tjunussov@gmail.com>
2018-04-04 22:16:44 +0600

    TJey

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,34 +1,82 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    {{#router}}
-    <router-view/>
-    {{else}}
-    <HelloWorld/>
-    {{/router}}
-  </div>
+<template lang="pug">
+doctype html
+
+#app
+  header.header.clearfix
+    b-container
+      b-navbar(toggleable="md")
+
+        b-nav-toggle(target="nav_collapse")
+
+        b-navbar-brand(to="/home")
+          i.fa.fa-building.text-muted
+
+        b-collapse(is-nav id="nav_collapse")
+
+              b-dropdown-divider
+              b-dropdown-item(to="/link3") 
+                .fa.fa-gear.mr-1/  Settings
+
+          b-navbar-nav.ml-auto
+
+
+            b-nav-item(to="/link1" v-b-tooltip.hover.auto title="Касса")
+              .fa.fa-calculator.mr-1
+
+            b-nav-item(to="/link2" v-b-tooltip.hover.auto title="История платежей")
+              .fa.fa-list.mr-1
+
+            b-nav-item-dropdown(right)
+              template(slot="button-content")
+                .fa.fa-user.mr-2.text-success
+                span Username
+              b-dropdown-item(to="/profile") Профиль
+              b-dropdown-divider
+              b-dropdown-header Войти как
+              b-dropdown-divider
+              b-dropdown-item(to="/logout") Выйти
+
+  main(role='main')
+    b-container
+      //- keep-alive
+      router-view
+  
+  b-container.pt-1
+    footer.footer.mt-2
+      b-row 
+        b-col
+          small.text-muted &copy; 2017 Company Name.
+
 </template>
 
 <script>
-{{#unless router}}
-import HelloWorld from './components/HelloWorld'
 
-{{/unless}}
 export default {
-  name: 'App'{{#router}}{{else}},
-  components: {
-    HelloWorld
-  }{{/router}}
+  name: 'app',
+  data(){
+    return {
+      isLoginError:null,
+      ui:{
+        search:null,
+        showModalLogin:false
+      }
+    }
+  },
+  methods:{
+    test(){
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+
 </style>
